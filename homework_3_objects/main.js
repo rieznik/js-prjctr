98460237f82eb1b3e5ff7ba3f980d1e389a4be4f
Kateryna Rieznik <rieznik.k@gmail.com>
2023-03-12 16:50:55 +0200

    Revert "add comment fixes"
    This reverts commit be5c8f246755c0c47ae41c8a91a9131313f6f35a.

--- homework_3_objects/main.js
+++ homework_3_objects/main.js
@@ -1,20 +1,20 @@
 // Duration between dates
 
-const durationBetweenDates = (start = '1 Jan 1970', end = Date(), units = 'days') => {
-  const startDate = new Date(start);
-  const endDate = new Date(end);
+const durationBetweenDates = (startDate = '1 Jan 1970', endDate = Date(), units = 'days') => {
+  const start = new Date(startDate);
+  const end = new Date(endDate);
 
-  const conversionDividers = {
-    seconds: 1000,
-    minutes: 60000,
-    hours: 3600000,
-    days: 86400000
-  }
-
-  if (conversionDividers[units]) {
-    return (Math.abs(startDate - endDate) / conversionDividers[units]).toFixed() + ' ' + units;
-  } else {
-    return 'Please, specify correct units. Available options: "seconds", "minutes", "hours", "days"';
+  switch (units) {
+    case 'seconds':
+      return (Math.abs(start - end) / 1000).toFixed() + ' ' + units;
+    case 'minutes':
+      return (Math.abs(start - end) / 1000 / 60).toFixed() + ' ' + units;
+    case 'hours':
+      return (Math.abs(start - end) / 1000 / 60 / 60).toFixed() + ' ' + units;
+    case 'days':
+      return (Math.abs(start - end) / 1000 / 60 / 60 / 24).toFixed() + ' ' + units;
+    default:
+      return 'Please, specify correct units. Available options: "seconds", "minutes", "hours", "days"';
   }
 }
 
@@ -44,14 +44,14 @@ console.log(updatedPriceData)    // {apples: '23.40', bananas: '48.00', oranges:
 // Recursively find sum of odd positive numbers until a specified number
 
 const recursiveOddSumTo = number => {
-  let n = number;
+  if (!(number % 2)) {
+    number = number - 1;
+  }
 
-  if (n <= 1) {
+  if (number <= 1) {
     return number;
-  } else if (n % 2) {
-    return n + recursiveOddSumTo(n - 2);
   } else {
-    return recursiveOddSumTo(n - 1);
+    return number + recursiveOddSumTo(number - 2);
   }
 }
 
@@ -62,16 +62,14 @@ console.log(recursiveOddSumTo(10)) // 25
 // Iteratively find sum of odd positive numbers until a specified number
 
 function iterativeOddSumTo(number) {
-  let n = number;
-
-  if (!(n % 2)) {
-    n--;
+  if (!(number % 2)) {
+    number = number - 1;
   }
 
   let sum = 0;
-  while (n > 0) {
-    sum += n;
-    n -= 2;
+  while (number > 0) {
+    sum += number;
+    number -= 2;
   }
 
   return sum;
