3cde5c2c7d4a04a51b97f94678ce7ed83981ef59
Kateryna Rieznik <rieznik.k@gmail.com>
2023-03-18 16:06:18 +0200

    turn off console rule

--- homework_1/main.js
+++ homework_1/main.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 // 1. Return Negative https://www.codewars.com/kata/55685cd7ad70877c23000102
 
 function makeNegative(number) {
--- homework_2_arrays/main.js
+++ homework_2_arrays/main.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-console */
 // Return sorted initials
 
 const userNames = [
