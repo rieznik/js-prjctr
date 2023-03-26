9872fea1c0bf44d6811e693b2d9b764750889d6d
Kateryna Rieznik <rieznik.k@gmail.com>
2023-03-26 13:22:15 +0300

    add solution for selectors task

---
+++ homework_5_dom/main.js
@@ -0,0 +1,6 @@
+//* 1. Selectors task
+
+const headingAboutNav = document.getElementById('headerTwo');
+const firstSection = document.getElementsByTagName('section');
+const fifthLi = document.querySelector('li:nth-child(5)');
+const elemantsWithClassText = document.getElementsByClassName('text');
