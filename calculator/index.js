c21a3eb797f39056fc184d4e60b6e85cee417935
Kateryna Rieznik <rieznik.k@gmail.com>
2023-04-03 19:53:59 +0300

    add click on numbers

---
+++ calculator/index.js
@@ -0,0 +1,21 @@
+`use strict`;
+
+const inputElement = document.querySelector('.input');
+
+const numbersElements = document.querySelectorAll('.number');
+
+const handleNumberClick = (event) => {
+  event.preventDefault();
+  console.log(event.target.innerHTML);
+  inputElement.value += event.target.innerHTML;
+};
+
+const startApp = () => {
+  console.log('Workshop2 startApp');
+
+  numbersElements.forEach((numberElement) =>
+    numberElement.addEventListener('click', handleNumberClick)
+  );
+};
+
+document.addEventListener('DOMContentLoaded', startApp);
