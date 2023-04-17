f015eac99b2ae4bfbbba6ee56478b50d1e924a1f
Kateryna Rieznik <rieznik.k@gmail.com>
2023-04-17 15:20:57 +0300

    add more classes

--- homework_6_oop1/index.js
+++ homework_6_oop1/index.js
@@ -32,26 +32,127 @@ class Infected extends Human {
   }
 }
 
-class Clicker extends Infected {
-  echolocate() {
-    console.log('kkkk...aarrrkkkk...kkrrrkkk');
+class Firefly extends Survivor {
+  constructor(name, age, abilities, role) {
+    super(name, age, abilities);
+    this.role = role;
+  }
+
+  communicate_with_group() {
+    console.log(`...${this.name} sends a message to the group...`);
   }
 }
 
-const ellie = new Survivor('Ellie', 14, [
-  'immune to Cordyceps',
-  'attack with switchblade',
-  'ride a horse',
-  'squeeze in tight entrance',
-]);
+class Smuggler extends Survivor {
+  constructor(name, age, abilities, cargo) {
+    super(name, age, abilities);
+    this.cargo = cargo;
+  }
 
-const clicker = new Clicker('three', [
-  'echolocation by rapid clicking and croaking sounds',
-  'enhanced strength',
-]);
+  negotiate() {
+    console.log(`...${this.name} asks for higher price...`);
+  }
+
+  showCargo() {
+    console.log(`Smuggling ${this.cargo}`);
+  }
+}
+
+class Clicker extends Infected {
+  constructor(infectionStage, abilities, isActiveFlag) {
+    super(infectionStage, abilities);
+    this.isActiveFlag = isActiveFlag;
+  }
+
+  echolocate() {
+    if (this.isActiveFlag) {
+      console.log('kkkk...aarrrkkkk...kkrrrkkk');
+      return;
+    }
+    console.log('This clicker is quietly standing');
+  }
+
+  isActive() {
+    console.log(
+      this.isActiveFlag ? 'Clicker is walking' : 'Clicker is standing still'
+    );
+  }
+}
+
+class Bloater extends Infected {
+  #sporesCloud = false;
+  #sporesTimer = 5000;
+
+  constructor(infectionStage, abilities, weight) {
+    super(infectionStage, abilities);
+    this.weight = weight;
+  }
+
+  launch_spore_cloud() {
+    console.log('...Explodes and releases spores...');
+    this.#sporesCloud = true;
+    setTimeout(() => {
+      this.#sporesCloud = false;
+      console.log('...Spores vanished...');
+    }, this.#sporesTimer);
+  }
+}
+
+const ellie = new Firefly(
+  'Ellie',
+  14,
+  [
+    'immune to Cordyceps',
+    'attack with switchblade',
+    'ride a horse',
+    'squeeze in tight entrance',
+  ],
+  'hope of humanity'
+);
+
+const joel = new Smuggler(
+  'Joel',
+  51,
+  [
+    'can smuggle anything',
+    'fierce fist-fighting',
+    'expert at wielding a wide range of weapons',
+    'ride a horse',
+    'swim',
+  ],
+  'Ellie'
+);
+
+const clicker = new Clicker(
+  'three',
+  ['echolocation by rapid clicking and croaking sounds', 'enhanced strength'],
+  true
+);
+
+const bloater = new Bloater(
+  'four',
+  [
+    'can withstand multiple hits from weapons',
+    'spraying the target with the toxin',
+    'releases spores',
+  ],
+  250
+);
 
 ellie.introduce();
 ellie.showAbilities();
+ellie.communicate_with_group();
+
+joel.introduce();
+joel.showAbilities();
+joel.negotiate();
+joel.showCargo();
 
 clicker.showAbilities();
+clicker.showInfectionStage();
 clicker.echolocate();
+clicker.isActive();
+
+bloater.showAbilities();
+bloater.showInfectionStage();
+bloater.launch_spore_cloud();
